aa8650756d2dbeac3a3a5818a75d74df72055b64
Indrajith <indrajith.bodinayaka352@gmail.com>
2024-12-17 15:59:41 +0530

    Add react hot toast to show notification title

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react"
 import { generateToken , messaging} from "./config/firebase"
+import toast, { Toaster } from 'react-hot-toast';
 import { onMessage } from "firebase/messaging";
 
 function App() {
@@ -8,12 +9,15 @@ function App() {
     console.log("hello")
     onMessage(messaging, (payload) => {
       console.log(payload)
+      toast(payload.notification.body)
     })
   },[])
 
   return (
     <div>
      <h1>Hello notification react app</h1>
+     <Toaster position="top-center"
+              reverseOrder={false}/>
     </div>
   )
 }
